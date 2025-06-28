f5ffa277511c49d3049f6dd7633171e9bbb4e4a6
Onkgopotse Molefe <onkgopotse.molefe@gmail.com>
2025-06-28 06:44:32 +0200

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -6,6 +6,7 @@ Script Purpose:
 This script creates tables in the 'bronze' schema, dropping existing tables
 if they already exist.
 Run this script to re-define the DDL structure of 'bronze' Tables
+UPDATED THE START AND END DATE DATATYPES
 ==========================================================================================================
 
 */
@@ -29,13 +30,14 @@ GO
 IF OBJECT_ID ('silver.crm_prd_info' , 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info
 CREATE TABLE silver.crm_prd_info(
-    prd_id INT,
+    	prd_id INT,
+	cat_id NVARCHAR(50),
 	prd_key NVARCHAR(50),
 	prd_nm NVARCHAR(50),
 	prd_cost INT,
 	prd_line NVARCHAR(50),
-	prd_start_dt DATETIME ,
-	prd_end_dt DATETIME ,
+	prd_start_dt DATE ,
+	prd_end_dt DATE ,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
